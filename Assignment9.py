79632e17d438e516d43485b06766d6b4f9c321ca
rohan22860 <rarora22860@gmail.com>
2018-08-28 10:37:37 +0530

    first commit

---
+++ Assignment9.py
@@ -0,0 +1,36 @@
+#Question 1
+try:
+    a=3
+    if a<4:
+        a=a/(a-3)
+        print(a)
+except ZeroDivisionError:
+    print("You Cannot Deivide By Zero")
+
+#Question 2
+    INDEX ERROR
+
+#Question 3
+    '''AN execption'''
+
+#Question 4
+    -5.0
+a/b result in 0
+
+#Question 5
+#import Error
+try:
+    import xyz
+except ImportError:
+    print("Please Import Valid Module")
+#value Error
+try:
+    a=int(input("Enter a Number ")
+except:
+    print("Please Enter a valid Number")
+#index Error
+try:
+    a=[1,2,3,]
+    print(a[4])
+except:
+    print("Please Enter A valid Index")
